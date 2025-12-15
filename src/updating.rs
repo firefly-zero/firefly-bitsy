e5dcfdf3f63d6018c084cd2b48628e2dbbcdcf83
gram <git@orsinium.dev>
2025-12-15 17:21:11 +0100

    handle walls

--- src/updating.rs
+++ src/updating.rs
@@ -35,12 +35,8 @@ fn move_avatar_to(state: &mut State, dx: i8, dy: i8) {
     let x = old_pos.x.saturating_add_signed(dx).min(TILES_X - 1);
     let y = old_pos.y.saturating_add_signed(dy).min(TILES_Y - 1);
     let new_pos = bs::Position { x, y };
-    let Some(room_id) = &avatar.room_id else {
-        return;
-    };
-    let room_id = room_id.clone();
 
-    if let Some(sprite) = get_sprite_at(state, &room_id, new_pos) {
+    if let Some(sprite) = get_sprite_at(state, new_pos) {
         return;
     }
 
@@ -57,16 +53,13 @@ fn get_avatar(state: &mut State) -> &mut bs::Sprite {
     panic!("avatar not found")
 }
 
-fn get_sprite_at<'a>(
-    state: &'a mut State,
-    room: &str,
-    pos: bs::Position,
-) -> Option<&'a bs::Sprite> {
+fn get_sprite_at<'a>(state: &'a mut State, pos: bs::Position) -> Option<&'a bs::Sprite> {
+    let room = &state.game.rooms[state.room];
     for sprite in &state.game.sprites {
         let Some(sprite_room) = sprite.room_id.as_ref() else {
             continue;
         };
-        if sprite_room != room {
+        if sprite_room != &room.id {
             continue;
         }
         if sprite.position != Some(pos) {
@@ -76,3 +69,20 @@ fn get_sprite_at<'a>(
     }
     None
 }
+
+// fn get_tile_at<'a>(state: &'a mut State, room: &str, pos: bs::Position) -> Option<&'a bs::tile> {
+//     let room = &state.game.rooms[state.room];
+//     for tile in &state.game.tiles {
+//         let Some(tile_room) = tile.room_id.as_ref() else {
+//             continue;
+//         };
+//         if tile_room != room {
+//             continue;
+//         }
+//         if tile.position != Some(pos) {
+//             continue;
+//         }
+//         return Some(tile);
+//     }
+//     None
+// }

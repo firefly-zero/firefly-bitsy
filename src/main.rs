64fae49933cc18f8270252db4050013d4005ee63
gram <git@orsinium.dev>
2025-12-15 12:28:40 +0100

    parse script

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #![no_std]
 #![no_main]
-use firefly_rust as ff;
+extern crate alloc;
+use alloc::borrow::ToOwned;
+use bitsy_nostd_parser::*;
+use firefly_rust::*;
 
 #[unsafe(no_mangle)]
 extern "C" fn boot() {
-    // ...
+    let raw = load_file_buf("main").unwrap();
+    let raw = alloc::str::from_utf8(raw.data()).unwrap();
+    let (game, _) = Game::from(raw.to_owned()).unwrap();
+    log_debug(&game.name);
 }
 
 #[unsafe(no_mangle)]
@@ -14,15 +20,5 @@ extern "C" fn update() {
 
 #[unsafe(no_mangle)]
 extern "C" fn render() {
-    ff::clear_screen(ff::Color::White);
-    ff::draw_triangle(
-        ff::Point { x: 60, y: 10 },
-        ff::Point { x: 40, y: 40 },
-        ff::Point { x: 80, y: 40 },
-        ff::Style {
-            fill_color: ff::Color::LightGray,
-            stroke_color: ff::Color::DarkBlue,
-            stroke_width: 1,
-        },
-    );
+    clear_screen(Color::White);
 }

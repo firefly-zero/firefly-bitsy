24f44a2caea0b09b94d420fad695b151e8d71364
gram <git@orsinium.dev>
2025-12-21 20:17:29 +0100

    dry dialog building

--- src/dialog.rs
+++ src/dialog.rs
@@ -9,79 +9,8 @@ pub struct Dialog {
 
 impl Dialog {
     pub fn new(dialog: &str, state: &mut bs::State, char_width: u8) -> Self {
-        const TRIPLE_QUOTE: &str = r#"""""#;
-        const LINES_PER_PAGE: usize = 2;
-        const LINE_WIDTH: usize = firefly_rust::WIDTH as usize;
-
-        // Remove triple quotes around the dialog
-        let mut dialog = dialog;
-        if let Some(new_dialog) = dialog.strip_prefix(TRIPLE_QUOTE) {
-            dialog = new_dialog.strip_suffix(TRIPLE_QUOTE).unwrap_or(dialog);
-        }
-
-        let tokenizer = bs::Tokenizer::new(dialog);
-        let interpreter = bs::Interpreter {
-            tokens: tokenizer,
-            state,
-        };
-
-        let mut pages = Vec::new();
-        let mut lines = Vec::new();
-        let mut words: Vec<bs::Word> = Vec::new();
-        let mut line_width = 0;
-        for word in interpreter {
-            use bitsy_script::Word::*;
-            match word {
-                LineBreak => {
-                    if !words.is_empty() {
-                        lines.push(Line { words });
-                        words = Vec::new();
-                        line_width = 0;
-                    }
-                    if lines.len() >= LINES_PER_PAGE {
-                        pages.push(Page { lines });
-                        lines = Vec::new();
-                    }
-                }
-                PageBreak => {
-                    if !words.is_empty() {
-                        lines.push(Line { words });
-                        words = Vec::new();
-                        line_width = 0;
-                    }
-                    if !lines.is_empty() {
-                        pages.push(Page { lines });
-                        lines = Vec::new();
-                    }
-                }
-                w => {
-                    let n_chars: usize = if let Text(t, _) = &w { t.len() } else { 8 };
-                    let word_width = n_chars * usize::from(char_width);
-                    if line_width + word_width > LINE_WIDTH {
-                        if !words.is_empty() {
-                            lines.push(Line { words });
-                            words = Vec::new();
-                            line_width = 0;
-                        }
-                        if lines.len() >= LINES_PER_PAGE {
-                            pages.push(Page { lines });
-                            lines = Vec::new();
-                        }
-                    }
-                    line_width += word_width;
-                    words.push(w)
-                }
-            }
-        }
-
-        if !words.is_empty() {
-            lines.push(Line { words });
-        }
-        if !lines.is_empty() {
-            pages.push(Page { lines });
-        }
-
-        Self { pages }
+        let builder = DialogBuilder::default();
+        builder.build(dialog, state, char_width)
     }
 
     pub fn n_pages(&self) -> usize {
@@ -105,4 +34,85 @@ pub struct Page {
 
 pub struct Line {
     pub words: Vec<bs::Word>,
+}
+
+#[derive(Default)]
+struct DialogBuilder {
+    pages: Vec<Page>,
+    lines: Vec<Line>,
+    words: Vec<bs::Word>,
+    line_width: usize,
+}
+
+impl DialogBuilder {
+    pub fn build(mut self, dialog: &str, state: &mut bs::State, char_width: u8) -> Dialog {
+        const TRIPLE_QUOTE: &str = r#"""""#;
+        const LINES_PER_PAGE: usize = 2;
+        const LINE_WIDTH: usize = firefly_rust::WIDTH as usize;
+
+        // Remove triple quotes around the dialog
+        let mut dialog = dialog;
+        if let Some(new_dialog) = dialog.strip_prefix(TRIPLE_QUOTE) {
+            dialog = new_dialog.strip_suffix(TRIPLE_QUOTE).unwrap_or(dialog);
+        }
+
+        let tokenizer = bs::Tokenizer::new(dialog);
+        let interpreter = bs::Interpreter {
+            tokens: tokenizer,
+            state,
+        };
+
+        for word in interpreter {
+            use bs::Word::*;
+            match word {
+                LineBreak => {
+                    self = self.flush_line();
+                    if self.lines.len() >= LINES_PER_PAGE {
+                        self = self.flush_page();
+                    }
+                }
+                PageBreak => {
+                    self = self.flush_line();
+                    if !self.lines.is_empty() {
+                        self = self.flush_page();
+                    }
+                }
+                w => {
+                    let n_chars: usize = if let Text(t, _) = &w { t.len() } else { 8 };
+                    let word_width = n_chars * usize::from(char_width);
+                    if self.line_width + word_width > LINE_WIDTH {
+                        self = self.flush_line();
+                        if self.lines.len() >= LINES_PER_PAGE {
+                            self = self.flush_page();
+                        }
+                    }
+                    self.line_width += word_width;
+                    self.words.push(w)
+                }
+            }
+        }
+
+        if !self.words.is_empty() {
+            self.lines.push(Line { words: self.words });
+        }
+        if !self.lines.is_empty() {
+            self.pages.push(Page { lines: self.lines });
+        }
+        Dialog { pages: self.pages }
+    }
+
+    fn flush_line(mut self) -> Self {
+        if !self.words.is_empty() {
+            self.lines.push(Line { words: self.words });
+            self.words = Vec::new();
+            self.line_width = 0;
+        }
+        self
+    }
+
+    fn flush_page(mut self) -> Self {
+        self.pages.push(Page { lines: self.lines });
+        self.lines = Vec::new();
+        self
+    }
 }
